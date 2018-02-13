a0ab9d03fbabe748d8b1b08c4c1d1988d0ef8536
Kate Marvel <kdm2144@columbia.edu>
2018-02-13 15:15:22 -0500

    CMIP5 penman-monteith test

--- penman_monteith.py
+++ penman_monteith.py
@@ -1,4 +1,34 @@
+import glob
+import sys
+import cdms2 as cdms
+import numpy as np
+import MV2 as MV
+import difflib
+import scipy.stats as stats
+global crunchy
+import socket
+import pickle
+if socket.gethostname().find("crunchy")>=0:
+    crunchy = True
+else:
+    crunchy = False
 
+#import peakfinder as pf
+import cdtime,cdutil,genutil
+from eofs.cdms import Eof
+from eofs.multivariate.cdms import MultivariateEof
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from Plotting import *
+import CMIP5_tools as cmip5
+import seasonal_cycle_utils as sc
+
+### Set classic Netcdf (ver 3)
+cdms.setNetcdfShuffleFlag(0)
+cdms.setNetcdfDeflateFlag(0)
+cdms.setNetcdfDeflateLevelFlag(0)
+def version_num(fname):
 def penmont_vpd_SH(Ta,Rnet,q,press,u):
     """
     
