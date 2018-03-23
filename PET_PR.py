8e503471c3dbc869fc9a6b43949dd4109acd4782
Kate Marvel <kdm2144@columbia.edu>
2018-03-23 16:52:17 -0400

    added phase calculations

--- PET_PR.py
+++ PET_PR.py
@@ -29,84 +29,112 @@ cdms.setNetcdfShuffleFlag(0)
 cdms.setNetcdfDeflateFlag(0)
 cdms.setNetcdfDeflateLevelFlag(0)
 
-# Read in PET and PR files
-#regridded PET
-f = cdms.open("OBS/cru_ts4.01.1901.2016.pet.dat.REGRID.nc")
-pet = f("pet")
-f.close()
-landmask = pet[0].mask
-# Regridded GPCC
-fpr = cdms.open("OBS/precip.mon.total.2.5x2.5.v7.nc")
-pr = fpr("precip")
-fpr.close()
-pr = pr/30. #Approximate mm-> mm/day by assuming 30 days/month
-#Put them both on the same time axis
-startpet = cmip5.start_time(pet)
-stoppet = cmip5.stop_time(pet)
-startpr = cmip5.start_time(pr)
-stoppr = cmip5.stop_time(pr)
 
-if cdtime.compare(startpet,startpr) >0:
-    start = startpet
-else:
-    start = startpr
-if cdtime.compare(stoppet,stoppr) >0:
-    stop = stoppr
-else:
-    stop = stoppet
-start = cdtime.comptime(start.year,start.month,1)
-stop = cdtime.comptime(stop.year,stop.month,31)
+def observations():
+    # Read in PET and PR files
+    #regridded PET
+    f = cdms.open("OBS/cru_ts4.01.1901.2016.pet.dat.REGRID.nc")
+    pet = f("pet")
+    f.close()
+    landmask = pet[0].mask
+    # Regridded GPCC
+    fpr = cdms.open("OBS/precip.mon.total.2.5x2.5.v7.nc")
+    pr = fpr("precip")
+    fpr.close()
+    pr = pr/30. #Approximate mm-> mm/day by assuming 30 days/month
+    #Put them both on the same time axis
+    startpet = cmip5.start_time(pet)
+    stoppet = cmip5.stop_time(pet)
+    startpr = cmip5.start_time(pr)
+    stoppr = cmip5.stop_time(pr)
 
-pr = pr(time=(start,stop))
-pet = pet(time=(start,stop))
+    if cdtime.compare(startpet,startpr) >0:
+        start = startpet
+    else:
+        start = startpr
+    if cdtime.compare(stoppet,stoppr) >0:
+        stop = stoppr
+    else:
+        stop = stoppet
+    start = cdtime.comptime(start.year,start.month,1)
+    stop = cdtime.comptime(stop.year,stop.month,31)
 
-#Calculate R and P
-Rpr,Ppr = sc.fast_annual_cycle(pr)
-Rpet,Ppet = sc.fast_annual_cycle(pet)
-#Convert phase to month of maximum (VECTORIZE THIS?)
-#How to handle "month of maximum" if it's fluctuating between 1 and 12?  (physically, what does this mean when our timesteps are every year?)
-#Should we modify the code in phase detection to start at phase 0?  Ie start in a month such that the maximum is 6 months away?
+    pr = pr(time=(start,stop))
+    pet = pet(time=(start,stop))
 
-#Calculate variance maps for p and pet
-test_period  = ('1979-1-1','2004-12-31') #for overlap with CMIP5 historical
-pet_vmap = sc.variance_map(pet(time=test_period))
-pr_vmap = sc.variance_map(pr(time=test_period))
+    #Calculate R and P
+    Rpr,Ppr = sc.fast_annual_cycle(pr)
+    Rpet,Ppet = sc.fast_annual_cycle(pet)
+    #Convert phase to month of maximum (VECTORIZE THIS?)
+    #How to handle "month of maximum" if it's fluctuating between 1 and 12?  (physically, what does this mean when our timesteps are every year?)
+    #Should we modify the code in phase detection to start at phase 0?  Ie start in a month such that the maximum is 6 months away?
 
-#make phase maps
-variance_threshold = 0.25 #Can we come up with a physically meaningful threshold here? Null hypothesis of no correlation ruled out at 99% confidence? 
-Ppet_clim = sc.phase_climatology(Ppet)
-Ppet_clim_month = sc.mask_data(sc.phase_to_month(Ppet_clim),landmask)
+    #Calculate variance maps for p and pet
+    test_period  = ('1979-1-1','2004-12-31') #for overlap with CMIP5 historical
+    pet_vmap = sc.variance_map(pet(time=test_period))
+    pr_vmap = sc.variance_map(pr(time=test_period))
 
-Ppr_clim = sc.phase_climatology(Ppr)
-Ppr_clim_month = sc.mask_data(sc.phase_to_month(Ppr_clim),landmask)
-months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
-from land_seasonal_cycle import landplot
-plt.subplot(211)
-m = landplot(MV.masked_where(pet_vmap<variance_threshold,Ppet_clim_month),cmap=cm.hsv,vmin=0,vmax=12)
-m.drawcoastlines(color='gray')
-cbar = plt.colorbar(orientation="horizontal")
-cbar.set_ticks(np.arange(12))
-cbar.set_ticklabels(months)
-plt.title("PET phase")
+    #make phase maps
+    variance_threshold = 0.25 #Can we come up with a physically meaningful threshold here? Null hypothesis of no correlation ruled out at 99% confidence? 
+    Ppet_clim = sc.phase_climatology(Ppet)
+    Ppet_clim_month = sc.mask_data(sc.phase_to_month(Ppet_clim),landmask)
 
-plt.subplot(212)
-m = landplot(MV.masked_where(pr_vmap<variance_threshold,Ppr_clim_month),cmap=cm.hsv,vmin=0,vmax=12)
-m.drawcoastlines(color='gray')
-cbar = plt.colorbar(orientation="horizontal")
-cbar.set_ticks(np.arange(12))
-cbar.set_ticklabels(months)
-plt.title("PR phase")
+    Ppr_clim = sc.phase_climatology(Ppr)
+    Ppr_clim_month = sc.mask_data(sc.phase_to_month(Ppr_clim),landmask)
+    months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
+    from land_seasonal_cycle import landplot
+    plt.subplot(211)
+    m = landplot(MV.masked_where(pet_vmap<variance_threshold,Ppet_clim_month),cmap=cm.hsv,vmin=0,vmax=12)
+    m.drawcoastlines(color='gray')
+    cbar = plt.colorbar(orientation="horizontal")
+    cbar.set_ticks(np.arange(12))
+    cbar.set_ticklabels(months)
+    plt.title("PET phase")
 
-#phase trends
-Pa_pet = sc.get_phase_anomalies(Ppet)
-Pa_pet_trends = cmip5.get_linear_trends(Pa_pet)
+    plt.subplot(212)
+    m = landplot(MV.masked_where(pr_vmap<variance_threshold,Ppr_clim_month),cmap=cm.hsv,vmin=0,vmax=12)
+    m.drawcoastlines(color='gray')
+    cbar = plt.colorbar(orientation="horizontal")
+    cbar.set_ticks(np.arange(12))
+    cbar.set_ticklabels(months)
+    plt.title("PR phase")
 
-Pa_pr = sc.get_phase_anomalies(Ppr)
-Pa_pr_trends = cmip5.get_linear_trends(Pa_pr)
+    #phase trends
+    Pa_pet = sc.get_phase_anomalies(Ppet)
+    Pa_pet_trends = cmip5.get_linear_trends(Pa_pet)
 
-
-
-
+    Pa_pr = sc.get_phase_anomalies(Ppr)
+    Pa_pr_trends = cmip5.get_linear_trends(Pa_pr)
 
 
 
+
+
+
+
+def phase_1pct(pet):
+    nmod,nt,nlat,nlon = pet.shape
+    Pmma = MV.zeros((nmod,nt/12,nlat,nlon))
+    for i in range(nmod):
+        R,P = sc.fast_annual_cycle(pet[i])
+        yax = P.getTime()
+        Pa = sc.get_phase_anomalies(P,historical=False)
+        Pmma[i]=Pa
+    Pmma.setAxis(0,pet.getAxis(0))
+    Pmma.setAxis(1,yax)
+    Pmma.setAxis(2,pet.getAxis(2))
+    Pmma.setAxis(3,pet.getAxis(3))
+    Pmma.id = pet.id+"_phase"
+    return Pmma
+    
+import string
+if __name__ == "__main__":
+    for variable in ["pet","pr","evspsbl"]:
+        fname = "/kate/TEST_DATA/"+string.upper(variable)+"_ensemble.nc"
+        f=cdms.open(fname)
+        writename="/kate/TEST_DATA/"+string.upper(variable)+"_phase.nc"
+        fw = cdms.open(writename,"w")
+        X = f(variable)
+        P = phase_1pct(X)
+        fw.write(P)
+        fw.close()
